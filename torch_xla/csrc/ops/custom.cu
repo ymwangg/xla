bf72db6dff19ac3cdac41754c27bbe0f9848bdc7
Yanming Wang <yanmwang@amazon.com>
2022-03-03 04:04:38 +0000

    Add custom index_select op

--- torch_xla/csrc/ops/custom.cu
+++ torch_xla/csrc/ops/custom.cu
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include "tensorflow/compiler/xla/service/custom_call_target_registry.h"
+#include "tensorflow/compiler/xla/xla_data.pb.h"
+#include <cuda.h>
 #include "custom.h"
+#include <iostream>
 
 __global__
 void saxpy(int n, float a, float *x, float *y)
@@ -41,3 +45,38 @@ void test()
   free(x);
   free(y);
 }
+
+__global__ void custom_call_kernel(const float* input, const int64_t* index, float* out, const int64_t len, const int64_t dim) {
+  const int n = 10;
+  for (size_t thread_id = n*threadIdx.x; thread_id+n < len; thread_id += n*blockDim.x) {
+    for (int k = 0; k < n; k++) {
+      int64_t idx = index[thread_id+k];
+      for (size_t i = 0; i < dim; i++) {
+        out[(thread_id+k)*dim + i] = input[idx*dim + i];
+      }
+    }
+  }
+}
+
+void do_custom_call(CUstream stream, void** buffers,
+                    const char* opaque, size_t opaque_len) {
+  const float* input = reinterpret_cast<const float*>(buffers[0]);
+  const int64_t* index = reinterpret_cast<const int64_t*>(buffers[1]);
+  float* output = reinterpret_cast<float*>(buffers[2]);
+  xla::ShapeProto shape;
+  shape.ParseFromArray(opaque, opaque_len);
+  // for (int i = 0; i < opaque_len; ++i) {
+  //   char c = opaque[i];
+  //   std::cout << c << std::endl;
+  // }
+  // for (int i = 0; i < shape.dimensions().size(); i++) {
+  //   std::cout << shape.dimensions(i) << std::endl;
+  // }
+  const int64_t len = shape.dimensions(0);
+  const int64_t dim = shape.dimensions(1);
+  const int64_t block_dim = 1024;
+  const int64_t grid_dim = 1;
+  custom_call_kernel<<<grid_dim, block_dim,
+                       /*dynamic_shared_mem_bytes=*/0, stream>>>(input, index, output, len, dim);
+}
+XLA_REGISTER_CUSTOM_CALL_TARGET(do_custom_call, "CUDA");
--- torch_xla/csrc/ops/index_select.cpp
+++ torch_xla/csrc/ops/index_select.cpp
@@ -34,7 +34,32 @@ NodePtr IndexSelect::Clone(OpList operands) const {
 XlaOpVector IndexSelect::Lower(LoweringContext* loctx) const {
   xla::XlaOp input = loctx->GetOutputOp(operand(0));
   xla::XlaOp index = loctx->GetOutputOp(operand(1));
-  return ReturnOp(xla::TorchIndexSelect(input, index, dim_), loctx);
+  bool use_custom_select =
+      xla::sys_util::GetEnvBool("XLA_CUSTOM_SELECT", false);
+  if (use_custom_select) {
+    const xla::Shape& input_shape = XlaHelpers::ShapeOfXlaOp(input);
+    const xla::Shape& index_shape = XlaHelpers::ShapeOfXlaOp(index);
+    std::vector<int64_t> dim;
+    for (int i = 0; i < index_shape.rank(); i++) {
+      dim.push_back(index_shape.dimensions(i));
+    }
+    dim.push_back(input_shape.dimensions(1));
+    // xla::XlaOp indices = xla::Zeros(
+    //     input.builder(),
+    //     xla::ShapeUtil::MakeShape(xla::PrimitiveType::F32, dim));
+    // return ReturnOp(indices, loctx);
+    std::string opaque;
+    xla::Shape shape = xla::ShapeUtil::MakeShape(xla::F32, dim);
+    shape.ToProto().SerializeToString(&opaque);
+    xla::XlaOp custom_call =
+        xla::CustomCall(loctx->builder(), "do_custom_call", /*operands=*/{input, index},
+                        /*shape=*/shape, opaque, false, /*output_operand_aliasing=*/{}, /*literal=*/nullptr,
+                        /*schedule=*/xla::CustomCallSchedule::SCHEDULE_EARLIEST);
+    return ReturnOp(custom_call, loctx);
+  }
+  else{
+    return ReturnOp(xla::TorchIndexSelect(input, index, dim_), loctx);
+  }
 }
 
 std::string IndexSelect::ToString() const {
